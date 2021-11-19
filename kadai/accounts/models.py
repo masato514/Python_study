d25ea255f994c882ded6558d3b2d424324a297a6
masato514 <94089019+masato514@users.noreply.github.com>
2021-11-19 16:18:33 +0900

    Delete kadai directory

--- kadai/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from django.contrib.auth.models import AbstractUser
-
-class CustomUser(AbstractUser):
-    """拡張ユーザーモデル"""
-
-    class Meta:
-        verbose_name_plural = 'CustomUser'
